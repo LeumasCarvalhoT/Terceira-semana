498dcc257a994ab1bfb0ed330fc8bbe32b1a2cba
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-02-06 12:41:49 +0000

    Thu, Feb 6, 2025, 9:41 AM -03:00

---
+++ mario/sn2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+//Validar a entrada de apenas numeros de 1 a 8
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Digite um numero de 1 a 8: ");
+
+    }while(n < 1 || n > 8);
+
+    for(int k = 0; k < n; k++)
+     {
+        for (int h = n - k; h > 0; h--)
+        printf(" ");
+        for(int h = 0; h <= k; h++)
+            printf("#");
+        printf("\n");
+
+
+   }
+
+   }
