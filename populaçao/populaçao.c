6624bec31c2af1b989d67561d1a0dbfe56085ba2
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-02-20 20:42:54 +0000

    Thu, Feb 20, 2025, 5:42 PM -03:00

--- populaçao/populaçao.c
+++ populaçao/populaçao.c
@@ -2,3 +2,10 @@
 #include <math.h>
 #include <stdio.h>
 
+int main (void)
+{
+    int populaçao_Atual = get_int ("Digite sua população atual de ilhamas: \n");
+    int populaçao_Final = get_int ("Agora sua população final: \n");
+
+    
+}
