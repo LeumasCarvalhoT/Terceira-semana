8bcd995418989be6cd0bb50901f4323ed5519e7a
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-02-28 20:39:47 +0000

    Fri, Feb 28, 2025, 5:39 PM -03:00

--- populaçao/populaçao.c
+++ populaçao/populaçao.c
@@ -20,8 +20,7 @@ printf("\n");
     do
     {
         populaçao_Final = get_int ("Agora sua população final de:");
-
-    } while (populaçao_Final <= populaçao_Atual);
+    } while (populaçao_Final < populaçao_Atual);
 printf("\n");
 
 int tempo = anos(populaçao_Atual,populaçao_Final);
@@ -35,10 +34,9 @@ int anos (int populaçao_Atual, int populaçao_Final)
 int anos_precisos = 0;
 
 if ( populaçao_Atual < populaçao_Atual )
-{
-    
-   int num_p = populaçao_Atual + ((populaçao_Atual / 3) - (populaçao_Atual / 4));
 
+{
+   int num_p = populaçao_Atual + ((populaçao_Atual / 3) - (populaçao_Atual / 4));
 }
 
 printf("Anos necessários para o aumento:%i \n",anos_precisos);
