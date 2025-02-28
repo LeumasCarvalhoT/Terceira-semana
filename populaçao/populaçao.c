430fd599b7babb5fac39aaac60d2088a14b1aaf8
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-02-28 21:12:55 +0000

    Fri, Feb 28, 2025, 6:12 PM -03:00

--- populaçao/populaçao.c
+++ populaçao/populaçao.c
@@ -8,7 +8,7 @@
 int main (void)
 {
 
-
+    int anos_precisos = 0;
     int populaçao_Atual;
     do
     {
@@ -24,11 +24,12 @@ printf("\n");
 printf("\n");
 
 
-int anos_precisos = 0;
-do{
-   int num_p = populaçao_Atual + (populaçao_Atual / 3) - (populaçao_Atual / 4);
+
+while (populaçao_Atual < populaçao_Final)
+{
+    int num_p = populaçao_Atual + (populaçao_Atual / 3) - (populaçao_Atual / 4);
     anos_precisos++;
-} while (populaçao_Atual < populaçao_Final);
+}
 
 printf("Anos necessários para o aumento:%i \n", anos_precisos);
 
