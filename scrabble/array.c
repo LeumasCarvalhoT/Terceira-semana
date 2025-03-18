5244ebcd86f95565ccddb949ef1e524e29a0c68f
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-03-18 00:08:56 +0000

    Mon, Mar 17, 2025, 9:08 PM -03:00

--- scrabble/array.c
+++ scrabble/array.c
@@ -9,7 +9,7 @@ int main(void)
    soma[0] = get_int ("x = ");
    soma[1] = get_int ("y = ");
 
-int  z =um[0] + dois[1]; //obrigatório colocar as chaves!!!
+int  z =soma[0] + soma[1]; //obrigatório colocar as chaves!!!
   printf ("resultado: %f\n", z / 2.0);
 
 }
