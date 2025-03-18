01f328ee8b59e9cf3b7508807f5fa40dcbe3986e
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-03-18 00:41:34 +0000

    Mon, Mar 17, 2025, 9:41 PM -03:00

--- scrabble/array0.c
+++ scrabble/array0.c
@@ -8,11 +8,11 @@ int main(void)
 int notas[total];
 
  for (int i = 0; i < total; i++)
-  {
+
     {
     notas[i] = get_int ("Nota: ");
     }
 
-  printf ("Sua média é: %i\n", notas [i] / total);
-}
+  printf ("Sua média é: %f\n",  notas[0], notas[1], notas[2] / 3.0);
+
 }
