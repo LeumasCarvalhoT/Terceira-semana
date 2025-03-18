43fd4cffb4188248818a5d01952a6ba60cffc55f
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-03-18 00:07:40 +0000

    Mon, Mar 17, 2025, 9:07 PM -03:00

--- scrabble/array.c
+++ scrabble/array.c
@@ -6,10 +6,10 @@
 int main(void)
 {
   int soma[2]; //indica o quantas variaveis tem
-   soma[0] = get_int ("x = ");
-   soma[1] = get_int ("y = ");
+   int um[0] = get_int ("x = ");
+   int dois[1] = get_int ("y = ");
 
-int  z =soma[0] + soma[1]; //obrigatório colocar as chaves!!!
+int  z =um[0] + dois[1]; //obrigatório colocar as chaves!!!
   printf ("resultado: %f\n", z / 2.0);
 
 }
