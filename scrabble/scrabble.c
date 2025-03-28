a1453ae379d282946e055557a245793d1a729aa3
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-03-28 19:57:04 +0000

    Fri, Mar 28, 2025, 4:57 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,7 +3,14 @@
 #include <ctype.h>
 #include <string.h>
 
+int pontos[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
 int main (void)
 {
-  int pontos[] = {1, 3, 3, 2, }
+  printf ("Digitem suas palavras.\n");
+
+string palavra1 = get_string ("jogador 1: \n");
+string palavra2 = get_string ("jogador 2: \n");
+
+
 }
