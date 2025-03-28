0a21dbddfcc0bbeb07f5cc62160437677e2de61c
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-03-28 20:49:33 +0000

    Fri, Mar 28, 2025, 5:49 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 
-int pontos[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int pontos[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int pc_pontuação (string palavra);
 
