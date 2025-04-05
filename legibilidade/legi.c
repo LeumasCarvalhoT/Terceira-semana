64d09401b720b05940e7ee146e880886f382b38c
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-04-05 00:32:02 +0000

    Fri, Apr 4, 2025, 9:32 PM -03:00

--- legibilidade/legi.c
+++ legibilidade/legi.c
@@ -7,7 +7,7 @@ int main(void)
 {
     string escrita = get_string ("Escreva seu texto: ")
 
-    letras = 0
-    palavras = 0
-    sentenças
+    int letras = 0;
+    int palavras = 1;
+    int sentenças = 0;
  }
