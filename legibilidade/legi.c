32623a720d5169876f2a63c72c0988d0ee271af2
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-04-05 01:32:50 +0000

    Fri, Apr 4, 2025, 10:32 PM -03:00

--- legibilidade/legi.c
+++ legibilidade/legi.c
@@ -31,7 +31,7 @@ printf ("\n");
         }
         n++;
     }
-printf("%s\n", escrita);
+printf("%i\n", caracteres);
 printf("Quantidade de letras: %i\n", letras);
 printf("Quantidade de palavras: %i\n", palavras);
 printf("Quantidade de sentenças: %i\n", sentenças);
