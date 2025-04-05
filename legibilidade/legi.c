8e9df605f543e1f3dc9e42dad11aabc392ffdbf6
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-04-05 00:37:06 +0000

    Fri, Apr 4, 2025, 9:37 PM -03:00

--- legibilidade/legi.c
+++ legibilidade/legi.c
@@ -10,4 +10,11 @@ int main(void)
     int letras = 0;
     int palavras = 1;
     int sentenças = 0;
+
+    int n = 0;
+
+    while (texto[n] != '\0')
+    {
+        int caracteres = tolower(escrita[n]);
+    }
  }
