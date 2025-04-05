e5107586ed71c32cd76cacdfe87ba3db2c987276
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-04-05 00:44:36 +0000

    Fri, Apr 4, 2025, 9:44 PM -03:00

--- legibilidade/legi.c
+++ legibilidade/legi.c
@@ -8,13 +8,21 @@ int main(void)
     string escrita = get_string ("Escreva seu texto: ")
 
     int letras = 0;
-    int palavras = 1;
+    int palavras = 0;
     int sentenças = 0;
 
     int n = 0;
 
-    while (texto[n] != '\0')
+    while (escrita[n] != '\0')
     {
         int caracteres = tolower(escrita[n]);
+        if(caracteres == 32)
+        {
+        palavras++;
+        }
+        else if(caracteres >= 'a' && caracteres <= 'z')
+        {
+            letras++;
+        }
     }
  }
