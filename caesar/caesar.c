ee3d02ffe469e046b877b3dc59f3339928069188
LeumasCarvalhoT <josukeparte8@gmail.com>
2025-04-11 21:18:01 +0000

    Fri, Apr 11, 2025, 6:18 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,50 +5,5 @@
 
 int main(void)
 {
-    string escrita = get_string ("Escreva seu texto: ");
-printf ("\n");
-
-   
-
-    int n = 0;
-
-    while (escrita[n] != '\0')
-    {
-        int caracteres = tolower(escrita[n]);
-        if(caracteres == 32)
-        {
-        palavras++;
-        }
-        else if(caracteres >= 'a' && caracteres <= 'z')
-        {
-            letras++;
-        }
-        else if (caracteres == '!' || caracteres == '?' || caracteres == '.')
-        {
-            sentenças++;
-        }
-        n++;
-    }
-
-printf("Quantidade de letras: %i\n", letras);
-printf("Quantidade de palavras: %i\n", palavras);
-printf("Quantidade de sentenças: %i\n", sentenças);
-
-float L =((float) letras / palavras) * 100;
-float S = ((float) sentenças / palavras) * 100;
-
-    int indice = 0.0588 * L - 0.296 * S - 15.8;
-
-   if(indice >= 16)
-   {
-    printf("Texto de 16º+ grau.\n");
-   }
-   else if(indice < 1)
-   {
-    printf("Antes do 1º grau.\n");
-   }
-   else
-   {
-    printf("Testo de %iº grau.\n", indice);
-   }
- }
+    
+}
